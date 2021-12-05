4b2713f7c90f50f6a48a8b6fff8a0ee60e0955e6
CKMolinga <ck.molinga@gmail.com>
2021-12-05 04:33:16 +0100

    Created a repository to manage course files

---
+++ Exercises/1_functionDeclaration.js
@@ -0,0 +1,16 @@
+// Function Declaration
+
+function greet() {
+    console.log("Hello")
+}
+
+// Call
+greet();
+
+// Function Expression
+const speak = function() {
+    console.log("Hello there");
+}
+
+// Call
+speak();
---
+++ Exercises/2_args_Params.js
@@ -0,0 +1,7 @@
+// Function Parameters
+const speak = function(name, time) {
+    console.log("${time,name}");
+}
+
+// Arguments
+speak('Mario', 'Good Morning');
